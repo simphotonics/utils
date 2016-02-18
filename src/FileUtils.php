6d7fc973b6506a829ef25070cff34d5d527c299d
D Reschner <git@simphotonics.com>
2016-02-18 13:59:22 +0000

    Added docblock to function loadFile. Changed return type of function write2file.

--- src/FileUtils.php
+++ src/FileUtils.php
@@ -11,8 +11,8 @@ class FileUtils
 {
     /**
      * Asserts that a file exists and is readable.
-     * @param  [type] $filename [description]
-     * @return [type]           [description]
+     * @param  string $filename File path.
+     * @return bool             Returns true on success.
      */
     public static function assertFileReadable($filename)
     {
@@ -21,49 +21,36 @@ class FileUtils
             throw new \RuntimeException("File: $filename not found!");
         }
         if (!is_readable($filename)) {
-            throw new \RuntimeException(" File: $filename not readable or non existing!");
+            throw new \RuntimeException(" File: $filename not readable!");
         }
         return true;
     }
     
     /**
-     * Checks that filenames referenced in attribute array
-     * point to existing, readable files.
-     * @param  Array  $attr
-     * @return bool
-     */
-    public static function assertRefFileReadable(array $attr)
-    {
-        $fileAttr = array('href', 'src');
-        foreach ($fileAttr as $key) { // Scan each resource path
-            if (isset($attr[$key])) {
-                if (!self::assertFileExists($attr[$var])) {
-                    throw new \RuntimeException("Attribute error! $key => $attr[$key]
-                        File: $attr[$key] does not exist!");
-                }
-            }
-        }
-        return true;
-    }
-
-    /**
      * Export string to file.
      * @param  string $filename
      * @param  string $mode
-     * @return 0    Exit code 1 on failure.
+     * @return int  Number of bites written.
+     *
+     * @throws \RuntimeException if file is not existing or writable.
      */
     public static function write2file($string, $filename, $mode = 'append')
     {
-        switch ($mode) {
-            case 'overwrite':
-                file_put_contents($filename, $string, LOCK_EX);
-                break;
-            default:
-                file_put_contents($filename, $string, FILE_APPEND);
-                break;
+        if (!is_writeable($filename) or is_dir($filename)) {
+            throw \RuntimeException("File $filename is not writable.");
         }
+        if ($mode === 'append') {
+            return file_put_contents($filename, $string, FILE_APPEND);
+        }
+        return file_put_contents($filename, $string, LOCK_EX);
     }
 
+
+    /**
+     * Imports file content.
+     * @param  string $fileName  File path.
+     * @return string            File content.
+     */
     public static function loadFile($fileName)
     {
         FileUtils::assertFileReadable($fileName);
